ca56e2f0f04219a3a80a88891c4c91dd4ccc7678
Pankajkumar2608 <tarachandji104@gmail.com>
2024-12-29 21:27:42 +0530

    added grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 
 import Hero from "./components/Hero";
+import { FloatingNav } from "./components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
+import { LampContainer } from "./components/ui/lamp";
+import Grid from "./components/Grid";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center bg-black text-white relative overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Hero />
+        <FloatingNav 
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+            {
+              name: "About",
+              link: "/about",
+              
+            },
+            {
+              name: "Projects",
+              link: "/projects",
+            },
+            {
+              name: "Contact",
+              link: "/contact",
+            },
+          ]}
+        />
+        
+         <Hero />
+        < Grid />
+        
       </div>
     </main>
   );
